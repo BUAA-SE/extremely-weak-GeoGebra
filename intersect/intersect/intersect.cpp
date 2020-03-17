05364ba4f5e740f720e392ab28e8a32d2695833b
MountVoom <491868399@qq.com>
2020-03-17 14:51:57 +0800

    new features

--- intersect/intersect/intersect.cpp
+++ intersect/intersect/intersect.cpp
@@ -1,20 +1,198 @@
-﻿// intersect.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include "intersect.h"
 
-#include <iostream>
+vector<Point> vec;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+bool pointOnSegment(const Point& A, const Line& L) {
+	if (L.tp == 'L') return true;
+	if (L.tp == 'R') {
+		return dcmp((A - L.u) * L.v) >= 0;
+	}
+	return dcmp((A - L.u) * (A - (L.u + L.v))) <= 0;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
+int lineIntersectionWithLine(const Line& L1, const Line& L2) {
+	if (!dcmp(L1.v ^ L2.v)) return 0;
+	Vector u = L1.u - L2.u;
+	double t = (L2.v ^ u) / (L1.v ^ L2.v);
+	globalIntersection = L1.u + L1.v * t;
+	return pointOnSegment(globalIntersection, L1) && pointOnSegment(globalIntersection, L2);
+}
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+void lineIntersectionWithCircle(const Line& L, const Circle& C) {
+	Point intersec;
+	double t1, t2;
+	double a = L.v.x, b = L.u.x - C.c.x, c = L.v.y, d = L.u.y - C.c.y;
+	double e = a * a + c * c, f = 2 * (a * b + c * d), g = b * b + d * d - C.r * C.r;
+	double delta = f * f - 4 * e * g;
+	if (dcmp(delta) < 0) return;
+	if (dcmp(delta) == 0) {
+		intersec = L.point(-f / (2 * e));
+		if (pointOnSegment(intersec, L))
+			vec.push_back(intersec);
+	}
+	else {
+		t1 = (-f - sqrt(delta)) / (2 * e); intersec = L.point(t1);
+		if (pointOnSegment(intersec, L)) vec.push_back(L.point(t1));
+		t2 = (-f + sqrt(delta)) / (2 * e); intersec = L.point(t2);
+		if (pointOnSegment(intersec, L)) vec.push_back(L.point(t2));
+	}
+}
+
+double calLength(Vector x) {
+	return sqrt(x * x);
+}
+
+double angle(Vector vec) {
+	return atan2(vec.y, vec.x);
+}
+
+void circleIntersectionWithCircle(const Circle& C1, const Circle& C2) {
+	double d = calLength(C1.c - C2.c);
+	if (dcmp(d) == 0)  return;
+	if (dcmp(C1.r + C2.r - d) < 0) return;
+	if (dcmp(fabs(C1.r - C2.r) - d) > 0) return;
+	double a = angle(C2.c - C1.c);
+	double da = acos((C1.r * C1.r + d * d - C2.r * C2.r) / (2 * C1.r * d));
+	Point p1 = C1.point(a - da), p2 = C1.point(a + da);
+	vec.push_back(p1);
+	if (p1 == p2) return;
+	vec.push_back(p2);
+}
+
+int solveBasic() {
+	//line & line
+	for (int i = 1; i <= cnt_l; ++i)
+		for (int j = i + 1; j <= cnt_l; ++j) {
+			if (lineIntersectionWithLine(line[i], line[j])) {
+				vec.push_back(globalIntersection);
+			}
+		}
+	//circle & circle
+	for (int i = 1; i <= cnt_c; ++i)
+		for (int j = i + 1; j <= cnt_c; ++j) {
+			circleIntersectionWithCircle(circle[i], circle[j]);
+		}
+	//line & circle
+	for (int i = 1; i <= cnt_l; ++i)
+		for (int j = 1; j <= cnt_c; ++j) {
+			lineIntersectionWithCircle(line[i], circle[j]);
+		}
+	sort(vec.begin(), vec.end());
+	auto new_end = unique(vec.begin(), vec.end());
+	vec.erase(new_end, vec.end());
+	return vec.size();
+}
+
+/*double calY(const Point& A, const Point& B, double x) {
+	//(x - A.x) * (y - B.y) = (x - B.x) * (y - A.y)
+	double k1 = x - A.x, k2 = x - B.x;
+	return (k1 * B.y - k2 * A.y) / (k1 - k2);
+}
+
+void getPoints() {
+	Point B;
+	double lx = -1e11, rx = 1e11, ly, ry;
+	for (int i = 1; i <= cnt_l; ++i) {
+		B = line[i].u + line[i].v;
+		if (!dcmp(line[i].u.x - B.x)) {
+			pq.push(Node(Point(B.x, rx), i, 0));
+			pq.push(Node(Point(B.x, lx), i, 1));
+			leftPoint[i] = Point(B.x, rx);
+		}
+		else {
+			ly = calY(line[i].u, B, lx);
+			ry = calY(line[i].u, B, rx);
+			pq.push(Node(Point(lx, ly), i, 0));
+			pq.push(Node(Point(rx, ry), i, 1));
+			leftPoint[i] = Point(lx, ly);
+		}
+	}
+}
+
+set<Point> pset;
+
+void updateInsertion(int x, int y) {
+	if (lineIntersectionWithLine(line[x], line[y])) {
+		if (pset.find(globalIntersection) == pset.end()) {
+			pset.insert(globalIntersection);
+			pq.push(Node(globalIntersection, x, -y));
+		}
+	}
+}
+
+int scanLine() {
+	getPoints();
+	while (!pq.empty()) {
+		auto node = pq.top(); pq.pop();
+		if (node.tp == 0) {
+			auto node2 = Node2(node.x, node.id);
+			tree.insert(node2);
+			auto it = tree.find(node2);
+			if (it != tree.begin()) {
+				updateInsertion(prev(it)->id, it->id);
+			}
+			if (next(it) != tree.end()) {
+				updateInsertion(it->id, next(it)->id);
+			}
+		}
+		else if (node.tp == 1) {
+			auto node2 = Node2(leftPoint[node.id], node.id);
+			auto it = tree.find(node2);
+			if (it != tree.begin() && next(it) != tree.end()) {
+				updateInsertion(prev(it)->id, next(it)->id);
+			}
+			tree.erase(it);
+		}
+		else {
+			//node.id -> pl -node.tp -> pr
+			auto pl = Node2(leftPoint[node.id], node.id);
+			auto pr = Node2(leftPoint[-node.tp], -node.tp);
+			auto itl = tree.find(pl);
+			auto itr = tree.find(pr);
+			int needswap = 0;
+			if (itl != tree.begin()) {
+				++needswap;
+				updateInsertion(prev(itl)->id, itr->id);
+			}
+			if (next(itr) != tree.end()) {
+				++needswap;
+				updateInsertion(itl->id, next(itr)->id);
+			}
+			if (needswap == 2) swap(leftPoint[itl->id], leftPoint[itr->id]);
+		}
+	}
+	return s.size();
+}*/
+
+int main(int argc, char* argv[])
+{
+	errno_t err;
+	FILE* stream;
+	for (int i = 0; i < argc; ++i) {
+		if (argv[i][0] == '-' && argv[i][1] == 'o')
+			err = freopen_s(&stream, argv[i + 1], "w", stdout);
+		if (argv[i][0] == '-' && argv[i][1] == 'i')
+			err = freopen_s(&stream, argv[i + 1], "r", stdin);
+	}
+	ios::sync_with_stdio(false);
+	Point A, B; char tp[10];
+	cin >> n;
+	for (int i = 1; i <= n; ++i) {
+		cin >> tp;
+		if (tp[0] == 'C') {
+			++cnt_c;
+			circle[cnt_c].c.read();
+			cin >> circle[cnt_c].r;
+		}
+		else {
+			++cnt_l;
+			A.read();
+			B.read();
+			line[cnt_l].u = A;
+			line[cnt_l].v = B - A;
+			line[cnt_l] = Line(A, B - A, tp[0]);
+		}
+	}
+	printf("%d\n", solveBasic());
+	return 0;
+}
---
+++ intersect/intersect/intersect.h
@@ -0,0 +1,105 @@
+#pragma once
+#include<set>
+#include<queue>
+#include<cstdio>
+#include<vector>
+#include<cstdlib>
+#include<iostream>
+#include<algorithm>
+#define mp make_pair
+#define pii pair<int, int>
+using namespace std;
+typedef long long LL;
+const double eps = 1e-12;
+const int maxn = 500010;
+
+inline int dcmp(double x) {
+	return (x > eps) - (x < -eps);
+}
+
+struct Point {
+	double x, y;
+	Point(double x = 0, double y = 0) : x(x), y(y) {}
+	void read() {
+		cin >> x >> y;
+	}
+	void print() {
+		printf("%.10lf %.10lf\n", x, y);
+	}
+	bool operator < (const Point& B) const { return dcmp(x - B.x) < 0 || (!dcmp(x - B.x) && dcmp(y - B.y) < 0); }
+}leftPoint[maxn];
+typedef Point Vector;
+
+Vector operator + (const Vector& A, const Vector& B) { return Vector(A.x + B.x, A.y + B.y); }
+Vector operator - (const Vector& A, const Vector& B) { return Vector(A.x - B.x, A.y - B.y); }
+double operator * (const Vector& A, const Vector& B) { return A.x * B.x + A.y * B.y; }
+Point operator * (const Vector& A, const double& p) { return Point(A.x * p, A.y * p); }
+bool operator == (const Point& A, const Point& B) { return !dcmp(A.x - B.x) && !dcmp(A.y - B.y); }
+double operator ^ (const Vector& A, const Vector& B) { return A.x * B.y - A.y * B.x; }
+
+struct Line {
+	char tp;
+	Point u;
+	Vector v;
+	Line() { tp = 'L';  }
+	Line(Point u, Vector v) :u(u), v(v) { tp = 'L';  }
+	Line(Point u, Vector v, char tp) :u(u), v(v), tp(tp) {}
+	Point point(double t) const {
+		return u + v * t;
+	}
+}line[maxn];
+
+struct Circle {
+	Point c;
+	double r;
+	Circle() {
+		c = Point(0, 0);
+		r = 0;
+	}
+	Circle(Point c, double r) :c(c), r(r) {}
+	Point point(double a) const {
+		return Point(c.x + cos(a) * r, c.y + sin(a) * r);
+	}
+}circle[maxn];
+
+Point globalIntersection;
+set<Point> s;
+int n, cnt_l, cnt_c;
+
+struct Node {
+	Point x;
+	int id, tp;//0->left 1->down <right -> intersec (line_{id} & line_{-tp})
+	Node(Point x, int id, int tp) : x(x), id(id), tp(tp) { }
+	bool operator < (const Node& A) const {
+		if (!dcmp(x.x - A.x.x)) {
+			if (!dcmp(x.y - A.x.y)) {
+				return tp > A.tp;
+			}
+			return dcmp(x.y - A.x.y) < 0;
+		}
+		return dcmp(x.x - A.x.x) > 0;
+	}
+};
+
+struct Node2 {
+	Point x;
+	int id;
+	Node2(Point x, int id) :x(x), id(id) {}
+	bool operator < (const Node2& A) const {
+		return !dcmp(x.y - A.x.y) ? dcmp(x.x - A.x.x) > 0 : dcmp(x.y - A.x.y) < 0;
+	}
+};
+priority_queue<Node> pq;
+set<Node2> tree;
+
+int scanLine();
+int solveBasic();
+void getPoints();
+double angle(Vector vec);
+double calLength(Vector x);
+void updateInsertion(int x, int y);
+bool pointOnSegment(const Point& A, const Line& L);
+double calY(const Point& A, const Point& B, double x);
+int lineIntersectionWithLine(const Line& L1, const Line& L2);
+void lineIntersectionWithCircle(const Line& L, const Circle& C);
+void circleIntersectionWithCircle(const Circle& C1, const Circle& C2);
